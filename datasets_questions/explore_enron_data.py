81b17fc9deee36c99ea73b918d7ad7aa10cac33e
Frederic Dhalleine <frederic.dhalleine@gmail.com>
2016-03-05 10:05:19 -0800

    Lesson 2: Results for SVM

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-""" 
+"""
     Starter code for exploring the Enron dataset (emails + finances);
     loads up the dataset (pickled dict of dicts).
 
@@ -12,11 +12,30 @@
     but here's an example to get you started:
 
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
-    
+
 """
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+
+# Question 1
+print "There are %d distinct people in the Enron dataset" % len(enron_data)
+
+# Question 2
+print "There are %d features for each person" % len(enron_data["SKILLING JEFFREY K"])
+
+# Question 3
+pois = 0
+for _, instance in enron_data.iteritems():
+  if instance['poi']:
+    pois += 1
+print "There are %d pois" % pois
+
+# Question 4
+print "Total stock value of James Prentice is %d" % enron_data["PRENTICE JAMES"]['total_stock_value']
+
+# Question 5
+print "There are %d emails from Wesley Colwell to poi" % enron_data["COLWELL WESLEY"]['from_this_person_to_poi']
 
 
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
-sys.path.append("../tools/")
+sys.path.append("/Users/fred/ownCloud/mooc/Udacity/Machine_Learning/ud120-projects/tools/")
 from email_preprocess import preprocess
 
 
@@ -24,6 +24,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+
+from sklearn.svm import SVC
+
+# Train on fewer examples
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+
+c = 10000.0
+classifier = SVC(kernel = 'rbf', C = c)
+classifier.fit(features_train, labels_train)
+
+print "mean accuracy for C=%f : %f" % (c, classifier.score(features_test, labels_test))
+
+print sum(classifier.predict(features_test))
 
 #########################################################
 
