ba4d8b7a23dd893fdea62fdc21e53bd2d44daf2b
Frederic Dhalleine <frederic.dhalleine@gmail.com>
2016-03-20 15:54:28 -0700

    Lesson 7: results for outliers

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -10,10 +10,31 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+
+sorted_data = {}
+for f in data_dict:
+  bonus = data_dict[f]['bonus']
+  if bonus:
+    sorted_data[bonus] = f
+
+import collections
+sorted_data = collections.OrderedDict(sorted(sorted_data.items()))
+for sd in sorted_data:
+  print "%s - %s" % (sorted_data[sd], sd)
+
+data_dict.pop('TOTAL')
 data = featureFormat(data_dict, features)
 
-
 ### your code below
+
+for point in data:
+  salary = point[0]
+  bonus = point[1]
+  matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
 
 
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -7,14 +7,24 @@ def outlierCleaner(predictions, ages, net_worths):
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
     ### your code goes here
 
-    
+    difference = predictions - net_worths
+    error = [e[0] for e in difference ** 2]
+    threshold = sorted(error)[-len(predictions)/10:][0]
+
+    for i, age in enumerate(ages):
+      worth = net_worths[i][0]
+      pred = predictions[i]
+      error = ((worth - pred) ** 2)[0]
+      if error < threshold:
+        cleaned_data.append((age[0], worth, error))
+
     return cleaned_data
 
